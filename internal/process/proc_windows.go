e88c56c7a6b8cb30b09cafce068f06c58b35c98d
fff <2750356943@qq.com>
2026-02-02 14:57:39 +0800

    internal/process: remove ignored Windows placeholder file (proc_windows.go)

--- internal/process/proc_windows.go
+++
@@ -1,6 +0,0 @@
-//go:build ignore
-
-// This file intentionally left as an ignored placeholder.
-// Platform-specific implementation has been removed; repository targets Linux only.
-
-package process
